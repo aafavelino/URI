da1493c1837174715ee8f19409e0bd2e1f14c66a
Adelino Afonso <adelino-afonso@hotmail.com>
2016-06-17 17:20:24 -0300

    Esfera

--- INICIANTE/1011.cpp
+++ INICIANTE/1011.cpp
@@ -3,9 +3,8 @@
 
 int main(int argc, char const *argv[])
 {
-	double raio = 0.0;
-	std::cin >> raio;
-
+	double x = 0.0, y = 0.0, z = 0.0;
+	std::cin >> x >> y >> z;
 
 	printf("VOLUME = %.3f\n", (3.14159 * pow(raio,3)*(4.0/3)));
 
