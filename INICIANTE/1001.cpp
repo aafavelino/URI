e02472b64a316b467e642eca01bab921c6ee4313
Adelino <Adelino@iMac-de-labcom-61.local>
2016-06-17 13:30:08 -0300

    Extremamente Básico

---
+++ INICIANTE/1001.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	int x1 = 0, x2 = 0;
+	std::cin >> x1;
+	std::cin >> x2;
+	std::cout << "X = " << x1 + x2 << std::endl;
+	return 0;
+}
