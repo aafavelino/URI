e07a42f648c9e3f4da848f37bf9ee9b330a4a520
Adelino Afonso <adelino-afonso@hotmail.com>
2016-06-17 15:50:41 -0300

    Produto Simples

---
+++ INICIANTE/1004.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	int x = 0, y = 0;
+	std::cin >> x;
+	std::cin >> y;
+
+	std::cout << "PROD = " << x * y << std::endl;
+
+	return 0;
+}
