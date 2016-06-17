73b65885b80b3f36a9b12e4bb433825d6841e445
Adelino Afonso <adelino-afonso@hotmail.com>
2016-06-17 17:33:10 -0300

    Distância Entre Dois Pontos

---
+++ INICIANTE/1015.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+int main(int argc, char const *argv[])
+{
+	float x1, x2, y1, y2;
+
+	std::cin >> x1 >> y1;
+	std::cin >> x2 >> y2;
+	float value = sqrt(pow((x2-x1),2) + pow((y2-y1),2));
+	printf("%.4f\n", value);
+
+	return 0;
+}
