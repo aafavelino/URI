3acd114a49bb180432696e8e478a78eabbc4f604
Adelino Afonso <adelino-afonso@hotmail.com>
2016-06-18 00:47:46 -0300

    Seis Números Ímpares

---
+++ INICIANTE/1070.cpp
@@ -0,0 +1,14 @@
+#include <iostrem>
+                                           
+	int main(int argc, char const *argv[])
+	{ 	
+	 	int n;
+	    scanf("%i",&n);
+	    for(int i = 1; i <= 6; i++){
+	        if(n%2 == 0)
+	            n += 1;
+	            printf("%i\n", n);
+	            n += 2;
+	    }
+	    return 0;
+   	}
