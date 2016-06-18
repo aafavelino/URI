f0c54de122bbb0efc26b467172c62f7743bd7c22
Adelino Afonso <adelino-afonso@hotmail.com>
2016-06-18 00:35:48 -0300

    Positivos e Média

---
+++ INICIANTE/1064.cpp
@@ -0,0 +1,22 @@
+#include <iostrem>
+                                           
+  int main(int argc, char const *argv[])
+  {
+      int i=1,g=0;
+      double n,media;
+      while(i<=6){
+          scanf("%lf", &n);
+          
+        
+          if(n>0){
+              g++;
+              media = (media +n);
+          }
+          
+         
+           i++;
+      }printf("%i valores positivos\n", g);
+      printf("%.1lf\n",media/g);
+      return 0;
+  }
+      
