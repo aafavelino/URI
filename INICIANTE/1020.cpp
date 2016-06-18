fe839ef83d3e9088bfd98b5e25f7f70d2dcec410
Adelino Afonso <adelino-afonso@hotmail.com>
2016-06-18 00:15:44 -0300

    Idade em Dias

--- INICIANTE/1020.cpp
+++ INICIANTE/1020.cpp
@@ -1,1 +1,13 @@
-1020.cpp
+#include <iostream>
+#include <cmath>
+
+int main(int argc, char const *argv[])
+{
+	int dias = 0;
+	std::cin >> dias;
+		std::cout << dias/365 << " ano(s)" << std::endl;
+		std::cout << dias%365/30 << " mes(es)" << std::endl;
+		std::cout << dias%365%30<< " dia(s)" << std::endl;
+	return 0;
+
+}
