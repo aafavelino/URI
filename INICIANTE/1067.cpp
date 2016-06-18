847c778edf806592ec657ef181651606f2043eba
Adelino Afonso <adelino-afonso@hotmail.com>
2016-06-18 00:44:00 -0300

    Números Ímpares

---
+++ INICIANTE/1067.cpp
@@ -0,0 +1,15 @@
+#include <iostrem>
+                                           
+int main(int argc, char const *argv[])
+{ 	
+ 	int n,i=1;
+    scanf("%i",&n);
+    if(n>=1 && n<=1000){
+	    while(i<=n){
+	        printf("%i\n", i);
+	        i=i+2;   
+	    }
+    }
+   	return 0;
+}
+    
