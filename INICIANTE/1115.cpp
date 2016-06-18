b1b52735809ba06ebdaf8e51b2a85315f335b7f9
Adelino Afonso <adelino-afonso@hotmail.com>
2016-06-18 01:03:24 -0300

    Quadrante

---
+++ INICIANTE/1115.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+                                           
+int main(int argc, char const *argv[])
+{
+    int x, y;
+    while(scanf("%d %d", &x, &y) != EOF && x != 0 && y != 0){
+        if (x > 0 && y > 0)
+            printf("primeiro\n");
+        else if (x < 0 && y < 0)
+            printf("terceiro\n");
+        else if (x < 0)
+            printf("segundo\n");
+        else
+            printf("quarto\n");
+    }
+    return 0;
+}
