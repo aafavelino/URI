a335be0db67857c4f7455e9657fd550fb133455b
Adelino Afonso <adelino-afonso@hotmail.com>
2016-06-18 00:27:11 -0300

    Números Pares

---
+++ INICIANTE/1059.cpp
@@ -0,0 +1,12 @@
+#include <iostrem>
+                                           
+	int main(int argc, char const *argv[])
+	{
+      int n=2;
+      while(n<=100){
+          printf("%i\n",n);
+          n+=2;
+      }
+      
+      return 0;
+   }
