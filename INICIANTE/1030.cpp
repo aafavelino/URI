9290dcc818bf33d9e792fbdfe4206bea94e05e2d
Adelino Afonso <adelino-afonso@hotmail.com>
2016-06-18 10:52:55 -0300

    Tomadas

---
+++ INICIANTE/1030.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+                                           
+int main(int argc, char const *argv[])
+{
+    int a,b,c,d;
+    int s;
+    scanf("%i %i %i %i",&a,&b,&c,&d);
+    
+    s=(a+b+c+d)-3;
+    
+    printf("%i\n",s);
+    return 0;
+}
