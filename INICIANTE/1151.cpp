7aac0196c54e5489bc8a6c7bf95cf6aa30a6c854
Adelino Afonso <adelino-afonso@hotmail.com>
2016-06-18 02:28:50 -0300

    Fibonacci Fácil

---
+++ INICIANTE/1151.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+                                           
+int main(int argc, char const *argv[])
+{
+    int n, a, b, x, i;
+    scanf("%d", &n);
+    if (n == 0){ printf("0\n"); return 0; }
+
+    a = 1;
+    b = 1;
+    printf("0");
+    for (i = 1; i < n; i++){
+        printf(" %d", a);
+        x = a;
+        a = b;
+        b = b + x;
+    }
+    printf("\n");
+}
