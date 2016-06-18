f759283a16412ea1c7b0446a9f724a76fb5750fe
Adelino Afonso <adelino-afonso@hotmail.com>
2016-06-18 10:53:17 -0300

    Tri-du

---
+++ INICIANTE/1933.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+                                           
+int main(int argc, char const *argv[])
+{
+    int a,b;
+    scanf("%i %i",&a,&b);
+    
+    if(a>b){
+    printf("%i\n",a);
+    }else{
+    printf("%i\n",b);
+    
+    }
+}
