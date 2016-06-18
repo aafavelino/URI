cd9dc4d405d618d148cb1c31b1bb7c725459d609
Adelino Afonso <adelino-afonso@hotmail.com>
2016-06-18 02:51:29 -0300

    Vitória e a Indecisão

---
+++ INICIANTE/1924.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+                                           
+int main(int argc, char const *argv[])
+{
+    char curso[100];
+    int i;
+    int n=0;
+    
+    scanf("%i",&i);
+    
+    
+    while(n<i){
+        scanf("%s", &curso);
+        n++;
+    }
+    printf("Ciencia da Computacao\n");
+    
+}
