f6de2f8b19c787bf8ddf12af4478b98b9a58fccd
Adelino Afonso <adelino-afonso@hotmail.com>
2016-06-18 02:52:26 -0300

    Tempo de jogo

---
+++ INICIANTE/1046.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+                                           
+int main(int argc, char const *argv[])
+{
+    int h1,h2;
+    int tempo=0;
+    scanf("%i %i",&h1,&h2);
+    
+    tempo= ((h2-h1)+24)%24;
+    if(tempo==0){
+        tempo = 24;
+    }
+    
+    printf("O JOGO DUROU %i HORA(S)\n",tempo);
+    
+}
