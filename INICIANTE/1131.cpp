75ef49e0fe0add74604d72287d3a1baf6a77691b
Adelino Afonso <aafavelino@ufrn.edu.br>
2016-06-22 11:36:55 -0300

    Grenais

---
+++ INICIANTE/1131.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    int g, i, gv = 0, iv = 0, e = 0, n = 0, r;
+
+    do {
+        scanf("%d %d", &i, &g);
+        n++;
+
+        if (i == g) e++;
+
+        if (i > g) {
+            iv++;
+        } else {
+            gv++;
+        }
+
+        printf("Novo grenal (1-sim 2-nao)\n");
+    } while (scanf("%d", &r) && r == 1);
+
+    printf("%d grenais\n", n);
+    printf("Inter:%d\n", iv);
+    printf("Gremio:%d\n", gv);
+    printf("Empates:%d\n", e);
+
+    if (iv > gv) {
+        printf("Inter venceu mais\n");
+    } else if (gv > iv) {
+        printf("Gremio venceu mais\n");
+    } else {
+        printf("Nao houve vencedor\n");
+    }
+return 0;
+}
