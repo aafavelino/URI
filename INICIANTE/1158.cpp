28ae6713f60e6c4a2adfd9409daee38c236ca8de
Adelino Afonso <aafavelino@ufrn.edu.br>
2016-06-22 11:27:42 -0300

    Soma de Impares consecutivos

---
+++ INICIANTE/1158.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	int n, x, y, s;
+    std::cin >> n;
+    while (n--) {
+        s = 0;
+        std::cin >> x;
+        std::cin >> y;
+
+        if (x % 2 == 0) x++;
+
+        for (int i = 0; i < y; i++, x += 2) s += x;
+
+        std::cout << s << std::endl;
+    }
+    return 0;
+}
